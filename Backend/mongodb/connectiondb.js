64d111bae959350b3055dd72e5eecb4dd26d76ca
afonsogt890 <afonsogt123tablet@gmail.com>
2023-02-17 18:56:23 +0000

    Database creation

---
+++ Backend/mongodb/connectiondb.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectdb = async() => {
+    const clientkey = "mongodb+srv://afonsolanca:LnlkftyYjnRtJH00@project.idumupc.mongodb.net/?retryWrites=true&w=majority"
+
+    try {
+        mongoose.set("strictQuery", false);
+        mongoose.connect(clientkey, () => {console.log("Connected to database!")},{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    } catch (Error) {
+        console.log(`Error: ${Error.message}`)
+        process.exit(1);
+    }
+}
+
+export default connectdb;
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 import express from "express"; //Library pra criar o codigo base da api
+import connectdb from "./mongodb/connectiondb.js";
 
 const app = express(); //Variavel pra chamar uma função do express
 const port = 3001
 
+connectdb();
 app.listen(port, console.log(`server port: ${port}`))
