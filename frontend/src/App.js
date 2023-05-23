b39662a70cfb66c4b60feec490ebff0ca30969f3
afonsogt890 <afonsogt123tablet@gmail.com>
2023-05-23 21:15:52 +0100

    Setup frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { HomePage } from "./Pages/homepage.js";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={HomePage} exact/>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
